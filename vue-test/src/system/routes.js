c9502550c0919f39b7d35721b06fa29abf320c6c
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-14 16:42:31 +0300

    Работа над: RoadMap.md. Работа над п. 12. Выполнен п. 12. 2. 2. В файл: routes.js добавлен путь до профиля пользователя. Дополнен план пунтком 12. 3 (12. 3. 1, 12. 3. 2).

--- vue-test/src/system/routes.js
+++ vue-test/src/system/routes.js
@@ -1,5 +1,7 @@
 import index from '../pages/index.vue'
+import userProfile from  '../pages/userProfile.vue'
 
 export const routes = [
   { path: '/', component: index },
+  { path: '/userProfile', component: userProfile }
 ]
