430e068c97673c5cba528b8c95022c6350ac0b07
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-14 16:57:18 +0300

    Работа над: RoadMap.md. Обновлён план, добавлен 12. 2. 3. Работа над п. 12. Выполнен п. 12. 2. 3. Добавлен необходимые импорты в main.js, использовал router в корневом компоненте.

--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './system/routes'
+
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
