b9ff9d9bb4f5933eb331f935e0474b843421f4d9
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-15 03:48:56 +0700

    Работа над RoadMap.md. Выполнен п. 14. 1. 1.  Создан компонент: navBar.vue с базовой разметкой и его стилевой файл: navBar.sass

--- vue-test/src/components/navBar.vue
+++ vue-test/src/components/navBar.vue
@@ -1,11 +1,18 @@
-<template>
-
+<template lang="pug">
+  div.nav-bar-wrapper
+    div.nav-bar
+      ul
+        li
+          router-link(tag="a" to="/") Назад
+    div.searh-users-block
+      input(type="text" placeholder="поиск по юзерам")
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+@import "../style/navBar"
+@import "../style/reset"
 </style>
