3681c95fcd217989e67ea38bb232a9f24dbfc083
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-18 06:18:44 +0700

    Работа над RoadMap.md. Поправил маршруты к компонентам, добавил имена и параметры к компонентам: 1. userList. 2. userListItem. 3. userDetails.

--- vue-test/src/system/routes.js
+++ vue-test/src/system/routes.js
@@ -1,7 +1,39 @@
 import index from '../pages/index.vue'
 import userProfile from  '../pages/userProfile.vue'
+import userList from '../components/userList.vue'
+import userListItem from '../components/userListItem.vue'
+import userDetails from '../components/userDetails.vue'
 
 export const routes = [
-  { path: '/', component: index },
-  { path: '/userProfile', component: userProfile }
+  {
+    path: '/',
+    name:'Index',
+    component: index
+  },
+
+  {
+    path: '/userProfile',
+    name:'userProfile',
+    component: userProfile
+  },
+
+  {
+    path: '/userList/:query',
+    name:'userList',
+    component: userList
+  },
+
+  {
+    path:'/userListItem/:id',
+    name:'userListItem',
+    component: userListItem
+  },
+
+  {
+    path:'/userDetails/:id',
+    name:'userDetails',
+    component: userDetails
+  }
+
+
 ]
