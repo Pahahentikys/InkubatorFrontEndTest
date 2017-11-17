9ec2dd1035445bbc6af452a078627dceaf851f0b
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-18 03:16:03 +0700

    Работа над RoadMap.md. Выполнил п. 14. 2. 1.

---
+++ vue-test/src/components/userListItem.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  div.user-list-item-wrapper
+    h1 Элемент из списка юзеров
+    <!--img.user-image {{user.photo_100}}-->
+    <!--p.user-first-name {{user.first_name}}-->
+    <!--p.user-last-name  {{user.last_name}}-->
+    <!--a(@click="userDetailsClicked") Details-->
+
+
+
+</template>
+
+<script>
+export default {
+  props:{
+    user: {
+      type: Object,
+    }
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    userDetailsClicked(){
+      this.$emit("userDetails", this.user.id);
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
