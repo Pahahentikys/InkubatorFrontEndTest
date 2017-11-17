3edd7f14be85feda8935d7bb3f245c87045b4efd
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-18 03:11:41 +0700

    Работа над RoadMap.md. Добавил пункты 14. 2, 14. 3.

---
+++ vue-test/src/components/userList.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div.user-list-wrapper
+    h1 Список юзеров
+    user-list-item
+
+
+
+</template>
+
+<script>
+  import UserListItem from "../components/userListItem.vue"
+
+  export default {
+    data(){
+      return{
+        users: []
+      }
+    },
+    components: {
+      UserListItem
+    },
+    methods:{
+      viewUserDetails(id){
+        let userForView =
+        this.$emit("viewUserDetails", "userForView")
+      }
+    }
+  }
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
