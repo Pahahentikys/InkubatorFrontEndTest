f86b0b4f863be33718c3484558474be04699a731
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-18 03:16:28 +0700

    Работа над RoadMap.md. Выполнил п. 14. 3.

---
+++ vue-test/src/components/userDetails.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div.user-details-wrapper
+    h1 Подробная информация о пользователе
+    //span {{user.first_name}}
+    //span {{user.first_name}}
+</template>
+
+<script>
+export default {
+  props:{
+    user: {
+
+    }
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
