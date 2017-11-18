961482d4ec09b65a1602772eff90a91ee59b4067
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-19 03:37:52 +0700

    Работа над RoadMap.md. Убрал мусор с компонента: index.vue

--- vue-test/src/pages/index.vue
+++ vue-test/src/pages/index.vue
@@ -6,10 +6,6 @@
     div.show-user-wrapper
       user-list
 
-    a(href="https://oauth.vk.com/authorize?client_id=6250888&redirect_uri=http://localhost:8080/&display=popup&response_type=token&state=hello&revoke=1")
-    input.access-token(type="text")
-    input(type="button" value="Найти пользователей" v-on:click="getUsers")
-
 </template>
 
 <script>
@@ -20,34 +16,6 @@
     name: 'index',
     components:{
       userList
-    },
-    data() {
-      return {
-        users: [],
-        token: 'access_token=dbabad03dbabad03dbabad03b6dbf4cc8bddbabdbabad0381a90d015c0132c6bfd42148',
-        apiVersion: '&v=5.69',
-        method: 'users.search'
-      }
-    },
-    methods: {
-      getUsers() {
-
-        axios.get(this.getURL(this.method),
-          {count: 10, fields: 'photo, first_name, last_name'}, {
-            headers: {
-              'X-Requested-With': 'XMLHttpRequest',
-              'Content-Type': 'application/json',
-              'Acces-Control-Allow-Origin': '*'
-            }
-          }
-        )
-          .then((response) => this.data = response.data.response)
-          .catch((error => console.log(error.response.data)))
-      },
-      getURL(method, params) {
-        return 'https://api.vk.com/method/' + method + '?' + params + '&' + this.token + '&' + this.apiVersion
-      },
-      getToken(){}
     }
   }
 
