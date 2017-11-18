58f2b8d14b6492cbaf34cfdccae496322591cee9
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-18 07:02:42 +0700

    Работа над RoadMap.md. Выполнен п. 17. 1.

--- vue-test/src/components/navBar.vue
+++ vue-test/src/components/navBar.vue
@@ -5,11 +5,19 @@
         li
           router-link(tag="a" to="/") Назад
     div.searh-users-block
-      input(type="text" placeholder="поиск по юзерам")
+      input(type="text" placeholder="поиск по юзерам" v-model="q")
+      router-link(tag="a", :to="{name: 'userList', params:{query: q}}") Поиск
+      span {{query}}
 </template>
 
 <script>
-
+export default {
+  data(){
+    return{
+      q:""
+    }
+  }
+}
 </script>
 
 <style lang="sass" scoped>
