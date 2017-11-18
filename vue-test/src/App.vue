ea46c4fd36038f00bcee1e3c5e98a61ab3e2b782
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-19 04:26:01 +0700

    Работа над RoadMap.md. Выполнил: п. 17 . 2. Создал запрос с захардкоженным тоукеном на получение 10 пользователей, с параметром, принятым из компонента: navBar.vue через axios в компонент userList.vue

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,19 +1,25 @@
 <template lang="pug">
   div
     nav-bar
-    router-view
+    router-view(v-bind:token="accessToken")
 </template>
 
 <script>
-import navBar from './components/navBar.vue'
+  import navBar from './components/navBar.vue'
 
-  export default{
-    components:{
+  export default {
+    components: {
       navBar
+    },
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        accessToken: "access_token=6cb73bab05de52dec76af86ae08f2826e79ba1d97dae34b649860dee21979618d8214715db3873832fba7",
+      }
     }
-}
+  }
 </script>
 
 <style lang="sass" scoped>
-@import "./style/reset"
+  @import "./style/reset"
 </style>
--- vue-test/src/components/userList.vue
+++ vue-test/src/components/userList.vue
@@ -1,36 +1,60 @@
 <template lang="pug">
   div.user-list-wrapper
     h1 Список юзеров
-    <!--user-list-item(user="{id: 3, first_name: 'kek'}")-->
     user-list-item(v-for="userItem in users", :user="userItem")
-    //span {{ userItem.id }}
-    //span {{ userItem.first_name }}
-    //span {{ userItem.last_name }}
+    input(type="button" value="Получить пользователей" v-on:click="getUsers")
+    h2 {{ token }}
 </template>
 
 <script>
   import UserListItem from "../components/userListItem.vue"
+  import axios from 'axios'
+
+  const apiVersion = 'v=5.69'
+  const method = 'users.search'
 
   export default {
+    props:['token'],
     data(){
       return{
+//        token: '',
         users: [
-          {
-            id: "1",
-            first_name : "ololo",
-            last_name: "ololoevich"
-          },
-          {
-            id: "2",
-            first_name : "ololo",
-            last_name: "memovich"
-          },
+//          {
+//            id: "1",
+//            first_name : "ololo",
+//            last_name: "ololoevich"
+//          },
+//          {
+//            id: "2",
+//            first_name : "ololo",
+//            last_name: "memovich"
+//          },
         ]
       }
     },
     components: {
       UserListItem
     },
+    methods:{
+      getUsers() {
+        axios.get(this.getURL(this.method),
+          {count: 10, fields: 'photo, first_name, last_name'}, {
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'X-Requested-With': 'XMLHttpRequest',
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+          .then((response) => this.data = response.data.response)
+          .catch((error => console.log(error.response.data)))
+
+        console.log(this.response);
+      },
+      getURL() {
+        return 'https://api.vk.com/method/' + method + '?' + 'q='+ this.$route.params['query'] + '&' + this.token + '&' + apiVersion
+      },
+    }
   }
 
 </script>
