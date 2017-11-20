e7994663b7e2ff7607bf67afe5685cfb90ddd9fb
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-21 02:09:55 +0700

    Работа над RoadMap.md. Закрыт п. 19, 19. 5. В компоненте userDetails.vue добавлен вывод подробной информации о пользователе.

--- vue-test/src/components/userDetails.vue
+++ vue-test/src/components/userDetails.vue
@@ -2,32 +2,43 @@
   div.user-list-item-wrapper
     h1 Элемент из списка юзеров
     router-link( tag="a", to="/" ) На главную
-    <!--img.user-image {{user.photo_100}}-->
-    //p.user-id  {{user.id}}
-    //p.user-first-name {{user.first_name}}
-    //p.user-last-name  {{user.last_name}}
-    <!--p.user-last-name  {{user.last_name}}-->
-    <!--a(@click="userDetailsClicked") Details-->
-    input(type="button", @click ="showId" value="Показать ID")
-
-
-
+    p.user-id {{user.uid}}
+    p.user-first-name {{user.first_name}}
+    p.user-last-name {{user.last_name}}
+    p.user-bdate {{user.bdate}}
+    p.user-education {{user.education}}
 </template>
 
 <script>
   export default {
-    data(){
-      return{
-
+    data() {
+      return {
+        user: {}
       }
     },
-    methods:{
-      userDetailsClicked(){
-        this.$emit("userDetails", this.user.id);
+    methods: {
+      getUser() {
+        const urlAPI = 'https://api.vk.com/method/'
+        const method = 'users.get?'
+        const UID = 'user_ids=' + this.$route.params['id']
+
+        let requestURL = urlAPI + method + UID
+
+        this.$jsonp(requestURL, {
+          fields: 'photo_100, first_name, last_name, bdate, city, country, education'
+        })
+          .then((resp) => {
+            console.log(resp)
+            this.user = resp.response[0]
+            console.log(this.user)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
       },
-      showId(){
-        console.log(this.$route.params['id'])
-      }
+    },
+    created(){
+      this.getUser()
     }
   }
 </script>
