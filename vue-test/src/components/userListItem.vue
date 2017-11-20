39fc1c7bf93defb23efc9ebc3cdb4684d5d9f02b
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-21 02:07:58 +0700

    Работа над RoadMap.md. Убрал мусор из компонента: userListItem.vue

--- vue-test/src/components/userListItem.vue
+++ vue-test/src/components/userListItem.vue
@@ -1,25 +1,15 @@
 <template lang="pug">
   div.user-details-wrapper
     h1 Подробная информация о пользователе
-      h2 {{user.id}}
+      h2 {{user.uid}}
       h2 {{user.first_name}}
       h2 {{user.last_name}}
-    router-link(tag="a", :to="{name: 'userDetails', params:{id: user.id}}") Подробнее
+    router-link(tag="a", :to="{name: 'userDetails', params:{id: user.uid}}") Подробнее
 </template>
 
 <script>
   export default {
     props: ['user'],
-    data(){
-      return{
-//        userItem: null
-      }
-    },
-    created(){
-      console.log(user);
-    }
-
-
   }
 </script>
 
