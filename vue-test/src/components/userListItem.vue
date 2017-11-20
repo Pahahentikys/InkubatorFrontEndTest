8ce5c8ce72741c0e30a6e0651ef36e662bd56804
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-21 03:33:13 +0700

    Работа над RoadMap.md. Поправил форматирование вывода данных о пользователе в компоненте: userListItem.vue

--- vue-test/src/components/userListItem.vue
+++ vue-test/src/components/userListItem.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   div.user-details-wrapper
     h1 Подробная информация о пользователе
-      h2 {{user.uid}}
-      h2 {{user.first_name}}
-      h2 {{user.last_name}}
+    img(:src="user.photo_100")
+    p {{user.uid}}
+    p {{user.first_name}}
+    p {{user.last_name}}
     router-link(tag="a", :to="{name: 'userDetails', params:{id: user.uid}}") Подробнее
 </template>
 
