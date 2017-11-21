e648d83c48c7a166f38d21c27395eeed592ba2f0
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-22 01:06:52 +0700

    Работа над RoadMap.md. Поправил вывод информации в: userListItem.vue

--- vue-test/src/components/userListItem.vue
+++ vue-test/src/components/userListItem.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   div.user-details-wrapper
-    h1 Подробная информация о пользователе
+    h1 Информация о пользователе
     img(:src="user.photo_100")
     p {{user.uid}}
     p {{user.first_name}}
     p {{user.last_name}}
-    router-link(tag="a", :to="{name: 'userDetails', params:{id: user.uid}}") Подробнее
+    router-link(tag="a", :to="{name: 'userDetails', params:{id: user.uid}}").text-details Подробнее
 </template>
 
 <script>
@@ -15,5 +15,6 @@
 </script>
 
 <style lang="sass" scoped>
-
+  .text-details
+    text-transform: uppercase
 </style>
