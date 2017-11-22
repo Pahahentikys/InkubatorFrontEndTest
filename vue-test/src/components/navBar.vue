a6a88b96a59a1a3d4792aaecf56db1e5fb7b8742
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-23 05:48:17 +0700

    Работа над RoadMap.md. Выполил п. 21. Добавил появление кнопки: "НАЗАД" по переходу в компонент: userDetails.vue и её сокрытие на всех остальных компонентах.

--- vue-test/src/components/navBar.vue
+++ vue-test/src/components/navBar.vue
@@ -1,26 +1,35 @@
 <template lang="pug">
   div.nav-bar-wrapper
     div.nav-bar
-      ul
-        li
-          router-link(tag="a" to="/") Назад
-    div.searh-users-block
-      input(type="text" placeholder="поиск по юзерам" v-model="q")
-      router-link(tag="a", :to="{name: 'userList', params:{query: q}}") Поиск
-      span {{query}}
+      div.searh-users-block
+        router-link(tag="a" to="/" v-show="currentRoute == 'userDetails'") Назад
+        input(type="text" placeholder="поиск по юзерам" v-model="q")
+        router-link(tag="a", :to="{name: 'userList', params:{query: q}}") Поиск
+        span {{query}}
 </template>
 
 <script>
-export default {
-  data(){
-    return{
-      q:""
+  export default {
+    data() {
+      return {
+        q: "",
+        currentRoute: this.$router.currentRoute.name
+      }
+    },
+    methods: {},
+    watch: {
+
+      '$route'(to, from) {
+        console.log('to', to.name)
+        console.log('from', from.name)
+        this.currentRoute = to.name
+      }
+
     }
   }
-}
 </script>
 
 <style lang="sass" scoped>
-@import "../style/navBar"
-@import "../style/reset"
+  @import "../style/navBar"
+  @import "../style/reset"
 </style>
