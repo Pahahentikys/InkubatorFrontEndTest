9e5c4ea3cc8d6a50033c695320a8c0c2b2218224
Pahahentikys <Pavel1995.rus@yandex.ru>
2017-11-23 06:03:43 +0700

    Работа над RoadMap.md. В файле main.js поправлено форматирование.

--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import { routes } from './system/routes'
+import {routes} from './system/routes'
 
 
 Vue.use(VueRouter)
